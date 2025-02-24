ddce847f7c7491b4a017a50520c9f587ea5ad2ff
lexterocava21 <lextercapule@gmail.com>
2025-02-24 19:27:49 +0800

    Add project files.

---
+++ Product/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Product
+{
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("Products");
+        }
+    }
+}
---
+++ Product/Entity/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Product
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProdName { get; set; }
+        public string ProdDesc { get; set; }
+        public int Quantity { get; set; }
+    }
+}
